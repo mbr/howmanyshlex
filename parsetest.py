287a79d119d8a6c3513d59d5e2bc6daaf336af6e
Marc Brinkmann <git@marcbrinkmann.de>
2015-07-26 22:03:59 +0200

    Initial import.

---
+++ parsetest.py
@@ -0,0 +1,61 @@
+import shlex
+
+
+class SyntaxError(Exception):
+    def __init__(self, reason, linenum, line):
+        super(SyntaxError, self).__init__('{}: {}'.format(linenum, reason))
+
+
+def parse_format(src):
+    rv = []
+
+    for n, line in enumerate(src.splitlines()):
+        # skip blank lines
+        if not line.strip():
+            continue
+
+        if line.startswith('  '):
+            # comment line
+            continue
+
+        fields = shlex.split(line, posix=True)
+
+        entry = {}
+
+        if len(fields) == 1:
+            raise SyntaxError('Missing password', n, line)
+
+        if len(fields) > 3:
+            raise SyntaxError('Too many entries', n, line)
+
+        entry['key'] = fields[0]
+        entry['username'] = None
+        if len(fields) == 2:
+            entry['password'] = fields[1]
+
+        elif len(fields) == 3:
+            entry['username'] = fields[1]
+            entry['username'] = fields[2]
+
+        rv.append(entry)
+
+    return rv
+
+
+sample = r"""
+.an_old_entry_that_is_ignored foo bar
+
+mail.google first-user@gmail.com "*****"
+  see https://mail.google.com/
+
+mail.google second-user@gmail.com "*****"
+  John's account
+
+ssh.my_private_server root "*****"
+  with great power comes great responsibility.
+
+mobile.pin 12345
+"""
+
+
+print parse_format(sample)
